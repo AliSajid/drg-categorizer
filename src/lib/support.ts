b55ff83f48deb274fd83adb743d3b63ccfaf4525
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-07-13 23:28:48 -0400

    refactor(support): refactor scaleLength to TS

--- src/lib/support.ts
+++ src/lib/support.ts
@@ -1,10 +1,11 @@
-export function scaleLength(length, ratio = 1) {
+export function scaleLength(length: string, ratio = 1) {
     const result = (length + "").trim().match(/^([\d.]+)([a-z]*)$/);
-  
+
     if (!result)
-      throw new TypeError("Invalid length, must be a number followed by a unit without spaces.");
-  
-    const [num, unit] = result.splice(1, 2);
-  
-    return `${num * ratio}${unit || "px"}`;
-  }
+        throw new TypeError("Invalid length, must be a number followed by a unit without spaces.");
+
+    const [numString, unit] = result.splice(1, 2);
+    const num = parseFloat(numString);
+
+    return `${num * ratio}${unit || "px"}`;}
+

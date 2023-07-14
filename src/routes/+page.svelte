d5f1b466b66a73bbfa89497a16b68e33d23628a3
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-07-13 23:22:03 -0400

    feat: fix the range of user ids

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
     // Generate a random number between 0 and 9
 
+    import Puff from '$lib/puff.svelte';
+
     async function get_random_user() {
-        const number = Math.floor(Math.random() * 10);
+        const number = Math.floor(Math.random() * 10) + 1;
         const user = await fetch(`https://jsonplaceholder.typicode.com/users/${number}`).then(r => r.json());
         return user
     }
@@ -15,7 +17,7 @@
     <h1>Welcome to SvelteKit</h1>
 
     {#await user}
-    <p>Waiting for User Data</p>
+    <Puff width="500px" height="500px" color="red" />
     {:then user}
     <p>The User of the Day is: {user.name}</p>
     <p>Their username is: {user.username}</p>

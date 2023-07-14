a2183e8a3688034d41c6d0c8af37550a64c50727
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-07-14 00:01:12 -0400

    style: change spacing of the puff loader

---
+++ src/lib/puff.svelte
@@ -0,0 +1,69 @@
+<script>
+    import { scaleLength } from "$lib/support";
+  
+    export let width = "100px";
+    export let height = "100px";
+    export let color = "currentColor";
+  
+    let aspectRatio = 1;
+  
+    if ($$props.width && !$$props.height) height = scaleLength(width, 1 / aspectRatio);
+    if ($$props.height && !$$props.width) width = scaleLength(height, aspectRatio);
+  </script>
+  
+  <!-- By Sam Herbert (@sherb), for everyone. More @ http://goo.gl/7AJzbL -->
+  <svg
+    {width}
+    {height}
+    stroke={color}
+    {...$$restProps}
+    viewBox="0 0 44 44"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g fill="none" fill-rule="evenodd" stroke-width="2">
+      <circle cx="22" cy="22" r="1">
+        <animate
+          attributeName="r"
+          begin="0s"
+          dur="1.8s"
+          values="1; 20"
+          calcMode="spline"
+          keyTimes="0; 1"
+          keySplines="0.165, 0.84, 0.44, 1"
+          repeatCount="indefinite"
+        />
+        <animate
+          attributeName="stroke-opacity"
+          begin="0s"
+          dur="1.8s"
+          values="1; 0"
+          calcMode="spline"
+          keyTimes="0; 1"
+          keySplines="0.3, 0.61, 0.355, 1"
+          repeatCount="indefinite"
+        />
+      </circle>
+      <circle cx="22" cy="22" r="1">
+        <animate
+          attributeName="r"
+          begin="-0.9s"
+          dur="1.8s"
+          values="1; 20"
+          calcMode="spline"
+          keyTimes="0; 1"
+          keySplines="0.165, 0.84, 0.44, 1"
+          repeatCount="indefinite"
+        />
+        <animate
+          attributeName="stroke-opacity"
+          begin="-0.9s"
+          dur="1.8s"
+          values="1; 0"
+          calcMode="spline"
+          keyTimes="0; 1"
+          keySplines="0.3, 0.61, 0.355, 1"
+          repeatCount="indefinite"
+        />
+      </circle>
+    </g>
+  </svg>
